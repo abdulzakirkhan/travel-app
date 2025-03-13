4832e31106f80f6df818dd8e49ec878e43635981
Abdul Zakir <128758587+abdulzakirkhan@users.noreply.github.com>
2025-03-13 10:40:04 +0500

    fixed navbar issues

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -75,11 +75,11 @@ const Navbar = () => {
                 <button className="text-gray-700 hover:text-red-600 focus:outline-none">
                   Visa
                 </button>
-                <div className="absolute left-0 mt-2 w-40 bg-white border rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
-                  <Link href="/visa-filling" className=" px-4 py-2 text-gray-700 hover:bg-gray-100">
+                <div className="absolute flex flex-col left-0 mt-2 !w-44 bg-white border rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
+                  <Link href="/visa-filling" className=" px-2 py-2 text-gray-700 hover:bg-gray-100">
                     VISAFILLING
                   </Link>
-                  <Link href="/visa-consultancy" className=" px-4 py-2 text-gray-700 hover:bg-gray-100">
+                  <Link href="/visa-consultancy" className=" ps-2 py-2 text-gray-700 hover:bg-gray-100">
                     VISA CONSULTANCY
                   </Link>
                 </div>
